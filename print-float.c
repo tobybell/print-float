772285710cbea6ab9463f7f3dd5a9392899f43f3
Toby Bell <tbell@stanford.edu>
2025-07-08 04:48:36 -0400

    WIP: trying to get working using exact integer math

--- print-float.c
+++ print-float.c
@@ -20,8 +20,72 @@ static void reverse(char* a, char* b) {
 
 // computes `a * 2^b / 10^c`
 static f64 calc(f64 a, i32 b, i32 c) {
-  a *= pow(2, b - c);
-  return c > 0 ? a / pow(5, c) : a * pow(5, -c);
+//  return a * exp2(b - c - c * log2(5));
+  a *= pow(2, b - c); // should be exact
+
+  // so now, if dividing by powers of 5, where does the loss of precision come from?
+  // when divide by 5, 
+
+  return c > 0 ? a / pow(5, c / 2) / pow(5, c - c / 2) : a * pow(5, -c / 2) * pow(5, -c + c / 2);
+}
+
+static u32 ceil2(u64 a, i32 b, i32 c) {
+  __uint128_t num = 1;
+  __uint128_t den = 1;
+
+  b -= c;
+  if (b > 0) {
+    if (64 + __builtin_clzll(a) < b) {
+      printf("b: %d\n", b);
+      abort();
+    }
+    num <<= b;
+  } else if (c < 0) {
+    __uint128_t p = 5;
+    u32 cc = (u32) -c;
+    while (cc) {
+      if (cc & 1)
+        num *= p;
+      p *= p;
+      cc /= 2;
+    }
+  }
+
+  if (b < 0) {
+    if (-b > 127) {
+      printf("-b: %d\n", -b);
+      abort();
+    }
+    den <<= -b;
+  } else if (c > 0) {
+    __uint128_t p = 5;
+    u32 cc = (u32) c;
+    while (cc) {
+      if (cc & 1)
+        den *= p;
+      p *= p;
+      cc /= 2;
+    }
+  }
+
+  __uint128_t ans = num / den + (a + den - 1 + num % den) / den;
+  printf("ya %d %d\n", b, c);
+  printf("%llu %llu\n", (u64) (num >> 64), (u64) num);
+  printf("%llu %llu\n", (u64) (den >> 64), (u64) den);
+  printf("%llu %llu\n", (u64) (ans >> 64), (u64) ans);
+  if (ans == 2)
+    abort();
+  if (ans != (u32) ans)
+    abort();
+  return (u32) ans;
+
+//  return a * exp2(b - c - c * log2(5));
+//  a *= pow(2, b - c); // should be exact
+
+  // so now, if dividing by powers of 5, where does the loss of precision come from?
+  // when divide by 5, 
+
+//  return c > 0 ? a / pow(5, c / 2) / pow(5, c - c / 2) : a * pow(5, -c / 2) * pow(5, -c + c / 2);
 }
 
 static i32 floor_div(i32 a, i32 b) { return a / b - (a % b && (a ^ b) < 0); }
@@ -80,16 +144,16 @@ static char* float_to_string(f32 x, char* buffer) {
   u32 a, b;
   if (sig_bits & 1) {  // odd, round away -> open interval
     a = (u32) floor(lo) + 1;
-    b = (u32) ceil(hi) - 1;
+    b = (u32) ceil2(2 * sig2 + 1, exp2 - 1, exp10) - 1;
   } else {  // even, round towards -> closed interval
-    a = (u32) ceil(lo);
+    a = (u32) (sig_bits ? ceil2(2 * sig2 - 1, exp2 - 1, exp10) : ceil2(4 * sig2 - 1, exp2 - 2, exp10));
     b = (u32) floor(hi);
   }
 
   u32 d = b / 10;  // power of 10 candidate
 
   u32 sig10;
-  if (d * 10 > a) {
+  if (d * 10 >= a) {
     sig10 = d;
     ++exp10;
   } else {
@@ -98,6 +162,9 @@ static char* float_to_string(f32 x, char* buffer) {
     // this would be the place to do it, using round(calc(sig2, exp2, exp10))).
     sig10 = b;
   }
+
+  if (!sig10)
+    abort();
 
   while (!(sig10 % 10)) {  // divide off extra 0s
     sig10 /= 10;
@@ -141,19 +208,21 @@ static void round_trip(u32 x) {
   f32 fy = strtof(buf, 0);
   u32 y;
   memcpy(&y, &fy, 4);
-  if (x != y)
+  if (x != y) {
+    printf("bad input\nfx %.9g\nfy %.9g\ns %s\n", fx, fy, buf);
     abort();
+  }
 }
 
 int main() {
   printf("checking all floats...\n");
   u32 max_norm = 0b11111111 << 23;
-  u32 pct = 0;
+  u32 pct = 27;
   u32 next = 0;
-  for (u32 i = 0; i < max_norm; ++i) {
+  for (u32 i = (u32) ((u64) max_norm * pct / 10000); i < max_norm; ++i) {
     if (i >= next) {
       printf("%u%%\n", pct);
-      next = (u32) ((u64) max_norm * ++pct / 100);
+      next = (u32) ((u64) max_norm * ++pct / 10000);
     }
     round_trip(i);
   }
